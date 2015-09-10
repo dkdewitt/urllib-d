9dda44bf985d00c56c660709e5aa678d7687ca40
David <dkdewitt@gmail.com>
2015-09-09 19:45:49 -0700

    Revert "Added fixes"
    This reverts commit 4ab1991d4ad8b556b06c7f1db4b277593e5761e0.

--- source/http/client.d
+++ source/http/client.d
@@ -7,32 +7,48 @@ import std.conv;
 import std.array;
 import std.concurrency;
 import core.thread;
+import std.string;
+import std.range;
+import http.common;
+import url.parser;
 
-private immutable string[] METHODS_REQUIRING_BODY = ["PATCH", "PUT", "POST"];
-enum HTTPVersion{
-    HTTP_1_1 = "HTTP/1/1"
-}
-
-
-void parseHeaders(){
-
-
-}
 
 
 class BaseHTTPConnection{
 protected:
     string sourceAddress;
-    string host;
+    string _host;
     ushort port;
     int timeout;
-    Socket sock;        
+    Socket sock;
 private:
+    ConnectionState defaultState = ConnectionState.CS_IDLE;
+    ConnectionState state;
+    ushort defaultPort = 80;
+    string defaultHTTPVersion = HTTPVersion.HTTP_1_1;
+    string[string] _headers;
+    HTTPResponse _response;
+    //char[] _response;
 
+    int debugLevel;
+    string method = "GET";
     
     
-    char[] _buffer;
+    string[] _buffer;
     
+    void output(string s){
+        _buffer ~= s;
+    }
+
+    /**
+    *   Send _buffer and optional message
+    **/
+    void sendBuffer(string message=null){
+
+    }
+
+
+
     void _sendRequest(string method, string url, string requestBody, string[string] headers ){
         string[] headerKeys = headers.keys;
 
@@ -63,45 +79,203 @@ private:
     }
 
     void _putHeader(string name, string value){
+
+        if(state != ConnectionState.CS_REQ_STARTED)
+            throw new CannotSendHeader("Request has not started");
+
         string header;
 
         header = name ~ " : " ~ value ~"\r\n";
-        char[] headerByte = header.dup;
+        string headerByte = header;
         //ubyte[] headerByte = cast(ubyte[]) header.dup;
 
         _buffer ~= headerByte;
 
     }   
 
+    void _outputToBuffer(string s){
+        _buffer ~= s;
+    }
+
+
+    void endHeaders(string messageBody=null){
+        if(state == ConnectionState.CS_REQ_STARTED)
+            state = ConnectionState.CS_REQ_SENT;
+        else
+            throw new CannotSendHeader("Request has not started");
+
+        _sendOutput(messageBody);
+    }
+    void _sendOutput(string messageBody = null){
+        auto buffer = this._buffer;
+        buffer ~= ["", ""];
+        auto message = buffer.join("\r\n");
+        this._buffer = null;
+        send(message);
+        if(messageBody)
+            send(messageBody);
+
+    }
+
     //void _
 
 public:
     this(string host, ushort port, int timeout=0, string sourceAddress=null){
-        this.host = host;
-        this.port = port;
+        _host = host;
+        port = port;
         timeout = timeout;
     }
 
-    void sendRequest(string method, string url, string requestBody, string[string] headers ){
+    void setHostPort(string host, string port=null){
+        if(port is null){
+            auto i = host.lastIndexOf(":");
+            auto j = host.lastIndexOf("]");
+            if(i > j){
+                try{
+                this.port  = to!ushort(host[i+1..$]);
+                } catch(ConvException exc){
+                    writefln("error message: %s", exc.msg);
+                    writefln("source file  : %s", exc.file);
+                    writefln("source line  : %s", exc.line);
+                    writeln();
+                }
 
+                this._host = host[0..i];
+            }
+            else
+                this.port = defaultPort;
+        }
+        else{
+            this.port = to!ushort(port);
+            this._host = host;
+        }
+    }
+
+
+    //Get Host w/o port
+    @property string host(){
+        string _host = _headers.get("host","");
+        auto colonSep = _host.lastIndexOf(":");
+        if (colonSep)
+            return _host[0..colonSep];
+        else
+            return _host;
+    }
+
+    void sendRequest(string method, string url, string requestBody, string[string] headers ){
+        _sendRequest(method,  url,  requestBody,  headers );
     }
 
     void connect(){
         sock = new TcpSocket(AddressFamily.INET);
-        writeln(this.host);
-        writeln(this.port);
-        sock.connect(new InternetAddress(this.host, this.port));
+        Address[] addresses = getAddress("www.google.com", 80);
+        sock.connect(addresses[0]);
+        sock.setOption(SocketOptionLevel.TCP, SocketOption.TCP_NODELAY,1);
+    }
+
+    void close(){
+        this.state = ConnectionState.CS_IDLE;
+        try{
+            Socket sock = this.sock;
+            if(sock){
+                
+                this.sock = null;
+                sock.close();
+            }
+        }
+        finally{    
+            HTTPResponse response = this._response;
+            if(response){
+                this._response = null;
+                response.close();
+            }
+
+        }
+    }
+
+    void send(string data){
+        if (this.sock is null){
+            //raise not connected maybe auto open??
+        }
+
+        if(debugLevel>0)
+            writeln("send: " ~ data);
+        size_t blockSize = 8192;
+
+        while(1){
+            writeln(sock.remoteAddress);
+            auto dataBlock = to!(char[])(drop(data, blockSize));
+            char[] x = to!(char[])(dataBlock);
+            //writeln();
+            if(dataBlock == null)
+                break;
+            writeln(dataBlock);
+            sock.send(cast(byte[]) dataBlock);
+            data = data[blockSize..$];
+            //break;
+
+        }
+        return;
+
     }
 
 
-    void send(char[] buff){
-        //char[] c  =  "GET / HTTP/1.1\r\nHost: localhost:9200\r\nConnection: keep-alive\r\nPragma: no-cache\r\nCache-Control: no-cache\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36\r\nAccept-Encoding: gzip, deflate, sdch\r\nAccept-Language: en-US,en;q=0.8\r\n\r\n".dup;
-        writeln(buff);
-        sock.send(buff);
-        char[1024] buf; 
-        receive();
+    /**
+    *   Send request to server
+    **/
+    void putRequest(string method, string url, int skip_host=0, int skipAcceptEncoding=0){
+        
+        if(this._response && this._response.isClosed())
+            this._response = null;
 
-    }
+        if (this.state == ConnectionState.CS_IDLE)
+            this.state = ConnectionState.CS_REQ_STARTED;
+        else
+            throw new CannotSendRequestException("Test");
+    
+        this.method = method;
+        if(url is null)
+            url = "/";
+
+        string request = format("%s %s %s", method, url , HTTPVersion.HTTP_1_1);
+        
+        //encode ascii??
+
+        if(this.defaultHTTPVersion == HTTPVersion.HTTP_1_1){
+            if(! skip_host){
+                URL netUrl;
+                if(url.startsWith("http")){
+                    //get URL obj
+                    netUrl = urlSplit(url, "http");
+               }
+               if(netUrl.netloc){
+                    _putHeader("Host", netUrl.netloc);
+                }
+               else{
+                /*if tunnel hsot
+
+                */
+
+                auto host = this._host;
+                auto port = this.port;
+
+                auto hostLoc = host.lastIndexOf(":");
+                if(hostLoc >= 0){
+                    //ipv6ify host
+                }
+
+                if(port == defaultPort){
+                    _putHeader("Host", host);
+                }
+
+               
+
+               }
+                
+            }
+        }
+    }        
+
 
     void receive(){
 
@@ -122,11 +296,65 @@ public:
     }
 
     unittest{
-        BaseHTTPConnection h1 = new BaseHTTPConnection("www.google.com", 80);    
+        BaseHTTPConnection h1 = new BaseHTTPConnection("localhost", 8085);    
         h1.connect();
         assert(h1.sock !is null);
 
         h1.send("Test Data".dup);
     }
 }
+
+
+class HTTPResponse{
+
+private:
+    Socket socket;
+    int debugLevel;
+    string method;
+    string url;
+
+public:
+
+    this(Socket sock, string method, string url,int debugLevel=0){
+        this.socket = sock;
+        this.debugLevel = debugLevel;
+        this.method = method;
+        this.url = url;
+    }
+
+
+    void close(){}
+
+
+    bool isClosed(){
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
